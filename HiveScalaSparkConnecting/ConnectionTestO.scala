f33b2c7aa2dedc69af8956af5d92a8e1a4d1f13a
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2022-06-27 12:50:40 -0400

    Additional comments on connecting.

--- HiveScalaSparkConnecting/ConnectionTestO.scala
+++ HiveScalaSparkConnecting/ConnectionTestO.scala
@@ -1,5 +1,5 @@
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.SparkSession // Must have hdfs running with yarn.
 
 object ConnectionTestO{ // Object file name.
   def main(args: Array[String]): Unit = {             //Need hadoop, scala2.13, hive and jdk8 installed.
@@ -15,7 +15,7 @@ object ConnectionTestO{ // Object file name.
     println("created spark session")
     /*Anything above this has been mostly unchanged (generalized)*/
 
-    val df2 = spark.read.format("csv").option("sep",",").load("hdfs://localhost:9000/user/hive/warehouse/Bev_BranchA.txt") // File location
+    val df2 = spark.read.format("csv").option("sep",",").load("hdfs://localhost:9000/user/hive/warehouse/Bev_BranchA.txt") // File location in running Hadoop
     df2.createOrReplaceTempView("beverage")                       //Type didn't seem to matter, just loaded straight .txt that looked like tables.
     spark.sql("SELECT * FROM beverage;").show()                     // Show() displays current tables.
     spark.sql("SELECT _c0 from beverage WHERE _c1='Branch6'").show() //Shows all tables that have Branch 6
