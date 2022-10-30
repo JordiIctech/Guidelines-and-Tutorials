8fbe7c1e8a4560673e7161d27eb29c409a688714
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2022-10-30 07:19:53 -0400

    Added select unique rows based on certain elements

--- SQL/online_practice.sql
+++ SQL/online_practice.sql
@@ -1,8 +1,10 @@
 /*All commands can be tested on: https://www.programiz.com/sql/online-compiler/ */
 
---1. Obtain all last names and count their repetations.
+--Obtain all last names and count their repetations.
 SELECT last_name, count(customer_id) AS Repetitions FROM Customers GROUP BY last_name;
 
 --Specific element count.
 SELECT COUNT(DISTINCT status) AS Repeats FROM Shippings;
 
+--Show only unique rows based on certain elements
+SELECT item, amount FROM Orders GROUP BY item, amount;
