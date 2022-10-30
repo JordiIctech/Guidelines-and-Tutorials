b1fceb10de76995f6320438ab9362c53256a3ed9
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2022-10-30 07:06:06 -0400

    Added small test

--- SQL/online_practice.sql
+++ SQL/online_practice.sql
@@ -1,3 +1,4 @@
-All commands can be tested on: https://www.programiz.com/sql/online-compiler/
+/*All commands can be tested on: https://www.programiz.com/sql/online-compiler/ */
 
-
+--1. Obtain all last names and count their repetations.
+SELECT last_name, count(customer_id) AS Repetitions FROM Customers GROUP BY last_name;
