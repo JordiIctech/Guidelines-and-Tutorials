0dfc5f71395a9fac357d00b1cfad55fbe9622dd4
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2022-11-11 17:46:02 -0500

    Dynamic variable method

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -1,1 +1,5 @@
-//Practice notes
+//Dynamic Variables
+for(let i = 1; i < 5; i++) { //let makes the variable a local variable
+    eval('var ' + "value" + i + '= ' + i + ';');
+    console.log(eval("value" + i)); //eval defines the variable based on the name and added number
+}
