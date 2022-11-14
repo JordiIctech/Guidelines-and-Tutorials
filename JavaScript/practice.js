16de9a1d1e25b603d46a9848ea2ec4869796bd6f
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2022-11-13 23:10:40 -0500

    Added string interpolation

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -1,5 +1,5 @@
 //Dynamic Variables
 for(let i = 1; i < 5; i++) { //let makes the variable a local variable
-    eval('var ' + "value" + i + '= ' + i + ';');
+    eval('var ' + `value${i}` + '= ' + i + ';'); //String interpolation, needs to use backticks (`)
     console.log(eval("value" + i)); //eval defines the variable based on the name and added number
 }
