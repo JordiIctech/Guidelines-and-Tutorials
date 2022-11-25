a5f7c71e24c31aef259e76b194021849f20d5a08
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2022-11-24 20:21:10 -0500

    If and added

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -3,3 +3,8 @@ for(let i = 1; i < 5; i++) { //let makes the variable a local variable
     eval('var ' + `value${i}` + '= ' + i + ';'); //String interpolation, needs to use backticks (`)
     console.log(eval("value" + i)); //eval defines the variable based on the name and added number
 }
+
+//if and
+if (coordsX < 265 && coordsY < 750){  
+    console.log("X", coordsX) 
+    console.log("Y", coordsY) }
