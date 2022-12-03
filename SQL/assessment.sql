356f6692bd17b08633bce6fc01fdda94769e53aa
JordiIctech <42590692+JordiIctech@users.noreply.github.com>
2022-12-02 21:29:19 -0500

    Q15 finished

--- SQL/assessment.sql
+++ SQL/assessment.sql
@@ -27,17 +27,45 @@
 --WHERE orderid < 10300 ORDER BY orderid ASC;
 
 ------------------------------------------------------------------------------------------------------10
+--SELECT COUNT(t1.ProductID) AS TotalNumber, t2.CategoryName 
+--FROM Products AS t1 
+--JOIN Categories AS t2 ON t1.CategoryID = t2.CategoryID
+--GROUP BY t2.CategoryName
+--ORDER BY COUNT(t1.ProductID) DESC;
 
-------------------------------------------------------------------------------------------------------11
+------------------------------------------------------------------------------------------------------11 (Recheck)
+--SELECT Country, City, COUNT(CustomerID) AS TotalCustomers FROM Customers
+--group BY City ORDER BY country ASC, COUNT(CustomerID) DESC;
 
 ------------------------------------------------------------------------------------------------------12
+--SELECT t1.EmployeeID, t1.LastName, t2.OrderID, t4.ProductName, t3.quantity FROM Employees t1
+--JOIN Orders t2 ON t1.employeeid = t2.employeeid
+--JOIN OrderDetails t3 ON t2.orderid = t3.orderid
+--JOIN Products t4 ON t3.productid = t4.productid
+--Order BY t2.OrderID ASC, t3.ProductID ASC;
 
 ------------------------------------------------------------------------------------------------------13
+--SELECT CustomerID FROM Customers
+--WHERE CustomerID NOT IN (SELECT CustomerID from Orders);
 
 ------------------------------------------------------------------------------------------------------14
+--Could also use LIMIT 10, but considering many orders tie, the rank() function was used.
+/*WITH 
+CTE1 AS (Select orderid, COUNT(DISTINCT(productid)) AS Lines,
+RANK() OVER (ORDER BY COUNT(DISTINCT(productid)) DESC) Ranked
+FROM OrderDetails GROUP BY orderid)
+SELECT * FROM CTE1 WHERE Ranked <= 10;*/
 
 ------------------------------------------------------------------------------------------------------15
-
+SELECT DISTINCT productid, OrderID, quantity
+FROM OrderDetails
+WHERE quantity > 60
+GROUP BY OrderID, quantity
+HAVING COUNT(*) > 1;
+--Add extra notes later.
+/*Accidentally double-entered a line item on an order, with a different ProductID, 
+but the same quantity. She remembers that the quantity was 60 or more. 
+Show all the OrderIDs with order details that match this, in order of OrderID.*/
 ------------------------------------------------------------------------------------------------------16
 
 ------------------------------------------------------------------------------------------------------17
